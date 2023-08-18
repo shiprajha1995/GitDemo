99635c251d42bbf6e3ac19f3d75976f7946e2d41
shiprajha1995 <shiprajha3017@gmail.com>
2023-08-18 23:55:58 +0100

    firstcode

---
+++ src/main/java/DeepTest/ShipTest/App.java
@@ -0,0 +1,13 @@
+package DeepTest.ShipTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
